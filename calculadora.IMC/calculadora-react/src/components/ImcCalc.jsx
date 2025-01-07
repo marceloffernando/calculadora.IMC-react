27d12ee4c8db3dc73183991da08e85156670a510
Marcelo Fernando <marceloffernando8@gmail.com>
2025-01-07 17:05:28 -0300

    form

--- calculadora.IMC/calculadora-react/src/components/ImcCalc.jsx
+++ calculadora.IMC/calculadora-react/src/components/ImcCalc.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+import Button from "./Button";
 import "./ImcCalc.css";
 
 const ImcCalc = () => {
+  const [altura, setAltura] = useState("");
+  const [weigth, setWeigth] = useState("");
+
+  const clearForm = (e) => {
+    e.prventDefault();
+    setAltura("");
+    setWeigth("");
+  };
+
+  // função para poder colocar só numeros sem letras
+  const digitosValidos = (text) => {
+    return text.replace(/[^0-9,]/g, "");
+  };
+
+  const handleAlturaChange = (e) => {
+    const updateValue = digitosValidos(e.target.value);
+    setAltura(updateValue);
+  };
+
+  const handleWeigthChange = (e) => {
+    const updateValue = digitosValidos(e.target.value);
+    setWeigth(updateValue);
+  };
+
   return (
     <div id="calc-container">
       <h2>calculadora IMC</h2>
@@ -13,6 +39,8 @@ const ImcCalc = () => {
               name="altura"
               id="altura"
               placeholder="exemplo 1,75"
+              onChange={(e) => handleAlturaChange(e)}
+              value={altura}
             />
           </div>
           <div className="form-control">
@@ -22,12 +50,14 @@ const ImcCalc = () => {
               name="weigth"
               id="weigth"
               placeholder="exemplo 70,5"
+              onChange={(e) => handleWeigthChange(e)}
+              value={weigth}
             />
           </div>
         </div>
         <div className="action-control">
-          <button>Calcular</button>
-          <button>Limpar</button>
+          <Button id="calc-btn" text="Calcular" />
+          <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
     </div>
