2e023b8878b35b2ad5f3fbb1f6aa9aedf7734098
Marcelo Fernando <marceloffernando8@gmail.com>
2025-01-08 10:51:05 -0300

    style

--- calculadora.IMC/calculadora-react/src/App.jsx
+++ calculadora.IMC/calculadora-react/src/App.jsx
@@ -1,11 +1,32 @@
+import { data } from "./data/data";
+import { useState } from "react";
 import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/ImcTable";
 import "./App.css";
 
 function App() {
+  const calcImc = (e, altura, weigth) => {
+    e.preventDefault();
+
+    if (!weigth || !altura) return;
+
+    // convenção de texto para numero
+    const weigthFloat = +weigth.replace(",", ".");
+    const alturaFloat = +altura.replace(",", ".");
+
+    const imcResult = (weigthFloat / (alturaFloat * alturaFloat)).toFixed(1);
+    setImc(imcResult);
+  };
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
   return (
     <>
       <div className="container">
-        <ImcCalc />
+        {/* se não tiver imc mostra(imcCalc) se tiver mostra (imcTable) */}
+        {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable data={data} />}
       </div>
     </>
   );
--- calculadora.IMC/calculadora-react/src/components/ImcCalc.jsx
+++ calculadora.IMC/calculadora-react/src/components/ImcCalc.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "./Button";
 import "./ImcCalc.css";
 
-const ImcCalc = () => {
+const ImcCalc = ({ calcImc }) => {
   const [altura, setAltura] = useState("");
   const [weigth, setWeigth] = useState("");
 
@@ -56,7 +56,11 @@ const ImcCalc = () => {
           </div>
         </div>
         <div className="action-control">
-          <Button id="calc-btn" text="Calcular" />
+          <Button
+            id="calc-btn"
+            text="Calcular"
+            action={(e) => calcImc(e, altura, weigth)}
+          />
           <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
